d6797055b96547b16fe94dd5059f6506e04d7409
Dhrumin100 <turing.ai50@gmail.com>
2025-07-29 20:36:06 +0530

    The   Google tag add in app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,18 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/images/logo.png" type="image/png" />
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-10841451989"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-10841451989');
+            `,
+          }}
+        />
       </head>
       <body className={inter.className}>
         <main>{children}</main>
